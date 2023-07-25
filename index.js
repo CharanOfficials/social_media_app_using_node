201c3ce511e6812dba8d1eba9936f1f8ea76280e
CharanOfficials <charanofficials@gmail.com>
2023-07-25 18:29:14 +0530

    Set the common styling for layouts

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import express from "express" // import express
 const app = express() // launching express
 const port = 8000 // port setup
 import router from './routes/index.js' // get the default route
+import expressLayouts from "express-ejs-layouts"
+
 app.set("view engine", "ejs") // set up a view engine
 app.set("views", "./views") // set the path
+app.use(expressLayouts) // set the layouts before routing starts
+app.use(express.static('./assets')) // entered in assets
 
 app.use('/', router); // set the default route
 
