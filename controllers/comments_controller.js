5f6f48f187d36750002504ca12ded3706af0ad38
CharanOfficials <charanofficials@gmail.com>
2023-08-09 10:56:19 +0530

    Posts owener can now delete the comments done by other users on its posts. Cooment owener can also delee theor comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -24,6 +24,37 @@ const createComment = function (req, res) {
     })
 }
 
+const destroy =  function (req, res) {
+    Comment.findById(req.params.id)
+        .then((comment) => {
+            let postId = comment.post
+            Post.findById(postId)  // To check whether post belongs to the logged in Usr so that they can delee the coments done by soe other user as well
+                .populate('user')  // Populate user stored in Post
+                .then(post => {
+                    return post.user
+                })
+                .then((postUser) => {
+                    if ((comment.user == req.user.id) || (postUser._id == req.user.id)) {
+                        comment.deleteOne()
+                        Post.findByIdAndUpdate(postId, { $pull: { comments: req.params.id } }, {new: true})
+                            .then((post) => {
+                                return res.redirect('back')
+                            })
+                            .catch((err) => {
+                                console.log("Unable to delete the comment from Post", err)
+                                return res.redirect('back')
+                            })
+                    }
+                })
+                .catch((err) => {
+                    console.log("Post User error", err)
+                })
+                }).catch((err) => {
+                    console.log("Error in finding the appropriate comment", err)
+                    res.redirect('back')
+    })
+}
+
 export default {
-    createComment
+    createComment, destroy
 }
--- routes/comments.js
+++ routes/comments.js
@@ -4,5 +4,5 @@ const router = express.Router()
 import commentsController from "../controllers/comments_controller.js"
 
 router.post('/create',passport.checkAuthentication ,commentsController.createComment)
-
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy)
 export default router
