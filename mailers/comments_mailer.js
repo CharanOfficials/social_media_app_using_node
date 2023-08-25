923e8b5cd220c42bba6eb3a20a12c61a25dcd8a0
CharanOfficials <charanofficials@gmail.com>
2023-08-25 13:41:14 +0530

    Setup the nodemailer for sending the email with html on adding a new comment against the Post

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -1,12 +1,12 @@
 import nodeMailer from "../config/nodemailer.js";
 
 const newComment = (comment) => {
-    console.log("Nodemailer")
+    let htmlString = nodeMailer.renderTemplate({comment: comment},'/comments/new_comment.ejs')
     nodeMailer.transporter.sendMail({
         from: 'codingninjas2k16@gmail.com',
         to: comment.user.email,
         subject: 'Your Comment Published',
-        html: '<h1>Your Comment is now published.</h1>'
+        html: htmlString
     }).then((info) => {
         console.log("Message sent", info)
         return
